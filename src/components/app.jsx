256cf141d4af24a5e57be112a5a015165266119c
Kamille Norris <kamionorato@gmail.com>
2020-12-28 22:45:32 -0500

    Add API key to env and fetch call

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { fetchWeather } from '../utils';
 import { ReactComponent as Logo } from "../images/sun.svg";
 import "../App.css";
 
 function App() {
+  fetchWeather("Frankfort");
   return <h1>Weather</h1>;
 }
 
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,11 @@
-const fetchWeather = cityName => {
-  const API_KEY = "<INSERT_API_KEY>";
-  // TODO: fetch weather forecast from endpoint
-  // from https://openweathermap.org/api
+export const fetchWeather = cityName => {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const BASE_URL = 'http://api.openweathermap.org';
+
+  if (cityName) {
+    return fetch(`${BASE_URL}/data/2.5/forecast?q=${cityName}&appid=${API_KEY}`)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
 };
